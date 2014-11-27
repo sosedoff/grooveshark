bbc54e1ae3e72d735fc3e394de4826ff1c09fb20
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2014-11-27 07:20:20 +0100

    Hash formatting

--- lib/grooveshark/song.rb
+++ lib/grooveshark/song.rb
@@ -31,13 +31,15 @@ module Grooveshark
 
     # Hash export for API usage
     def to_hash
-      { 'songID'      => @id,
+      {
+        'songID'      => @id,
         'songName'    => @name,
         'artistName'  => @artist,
         'artistID'    => @artist_id,
         'albumName'   => @album,
         'albumID'     => @album_id,
-        'track'       => @track }
+        'track'       => @track
+      }
     end
   end
 end
